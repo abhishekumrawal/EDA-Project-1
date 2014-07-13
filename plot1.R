27c575a81880bac04142252715da0a54886f48d3
abhishekumrawal <abhishekumrawal@gmail.com>
2014-07-13 15:05:39 +0530

    Revert "Fourth Commit!"
    This reverts commit 22a08063d685f50a425595af60f43a015c0edee0.

--- plot1.R
+++ plot1.R
@@ -1,21 +1,9 @@
-
-#Reading data from a text file.
-data = read.table("household_power_consumption.txt", header=TRUE,sep=";")
-
-#Changing character date variable to date format variable. 
-data$Date <- strptime(as.character(data$Date), "%d/%m/%Y")
-data$Date<-format(data$Date, "%Y-%m-%d")
-data$Date<-as.Date(data$Date)
-
-#Subsetting data for the required period.
-subdata<-subset(data, Date >= "2007-02-01")
+d = read.table("household_power_consumption.txt", header=TRUE,sep=";")
+d$Date <- strptime(as.character(d$Date), "%d/%m/%Y")
+d$Date<-format(d$Date, "%Y-%m-%d")
+d$Date<-as.Date(d$Date)
+subdata<-subset(d, Date >= "2007-02-01")
 subdata<-subset(subdata, Date <= "2007-02-02")
-
-#Creating a png grphic device.
-png(filename = "plot1.png",width = 480,height = 480,units = "px")
-
-#Constructing the histogram for Global Active Power.
-hist(as.numeric(as.character(subdata$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power (Kilowatts)")
-
-#switching the device off.
-dev.off()
+subdata$Global_active_power<-as.numeric(subdata$Global_active_power)
+#Plotting
+hist(subdata$Global_active_power/500,col="red",main="Global Active Power",xlab="Global Active Power (Kilowatts)")
--- plot2.R
+++ plot2.R
@@ -1,22 +1,9 @@
-
-#Reading data from a text file.
-data = read.table("household_power_consumption.txt", header=TRUE,sep=";")
-
-#Changing character date variable to date format variable. 
-data$Date <- strptime(as.character(data$Date), "%d/%m/%Y")
-data$Date<-format(data$Date, "%Y-%m-%d")
-data$Date<-as.Date(data$Date)
-
-#Subsetting data for the required period.
-subdata<-subset(data, Date >= "2007-02-01")
+d = read.table("household_power_consumption.txt", header=TRUE,sep=";")
+d$Date <- strptime(as.character(d$Date), "%d/%m/%Y")
+d$Date<-format(d$Date, "%Y-%m-%d")
+d$Date<-as.Date(d$Date)
+subdata<-subset(d, Date >= "2007-02-01")
 subdata<-subset(subdata, Date <= "2007-02-02")
-
-#Creating a png grphic device.
-png(filename = "plot2.png",width = 480,height = 480,units = "px")
-
-#Plotting Global Active Power series against weekdays of the date.
-plot(as.numeric(as.character(subdata$Global_active_power)),type="l",xlab="",ylab="Global Active Power (Kilowatts)",xaxt="n")
-axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
-
-#switching the device off.
-dev.off()
+subdata$Global_active_power<-as.numeric(subdata$Global_active_power)
+#Plotting
+plot(subdata$Global_active_power/500,type="l",xlab="",ylab="Global Active Power (Kilowatts)")
